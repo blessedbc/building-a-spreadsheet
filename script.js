cc954fce694a3d5cf989026827cda258f13fbcf5
ikenna nwaka <inwaka39@gmail.com>
2025-07-14 21:04:52 +0200

    middle

--- script.js
+++ script.js
@@ -20,4 +20,5 @@ const average = nums => sum(nums) / nums.length;
 const median = nums => {
   const sorted = nums.slice().sort((a, b) => a - b);
   const length = sorted.length;
+  const middle = length / 2 - 1;
 }
