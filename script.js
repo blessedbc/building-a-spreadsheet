e15ae858b57d0f0ea60b7faeaeb24ab6abbbe935
ikenna nwaka <inwaka39@gmail.com>
2025-07-20 20:35:49 +0200

    letters

--- script.js
+++ script.js
@@ -73,5 +73,10 @@ window.onload = () => {
   const container = document.getElementById("container");
    const createLabel = (name) => {
     const label = document.createElement("div");
+    label.className = "label";
+    label.textContent = name;
+    container.appendChild(label);
    }
+   const letters = charRange("A", "J");
+   letters.forEach(createLabel);
 }
